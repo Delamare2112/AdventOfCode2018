992a84f1c83b742432b197c3955b0862ad3cfba7
Trevor Berninger <Baladashman@gmail.com>
2018-12-01 10:48:44 -0800

    Day 1

---
+++ Day_1/main.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def part1(path):
+    frequency = 0
+    with open(path) as file:
+        for line in file:
+            frequency = eval("frequency + " + line)
+    print(frequency)
+
+
+def part2(path):
+    frequencies = [0]
+    frequency = 0
+    while True:
+        with open(path) as file:
+            for line in file:
+                frequency = eval("frequency + " + line)
+                if frequency in frequencies:
+                    print(frequency)
+                    return
+                frequencies.append(frequency)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A very unsafe day one solution. :)')
+    parser.add_argument('input', type=str)
+    args = parser.parse_args()
+
+    part1(args.input)
+    part2(args.input)
