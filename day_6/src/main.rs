5872e6379b05af6270382203551914a648b823a9
Trevor Berninger <trevor@lightfieldlab.com>
2018-12-26 09:30:31 -0800

    working with test input and debugging

---
+++ day_6/src/main.rs
@@ -0,0 +1,84 @@
+use trev_aoc_common::run;
+use regex::Regex;
+
+#[derive(Debug)]
+struct Point {
+    x: isize,
+    y: isize
+}
+
+fn taxi_dist(a: &Point, b: &Point) -> usize {
+    let p = Point {
+        x: a.x - b.x,
+        y: a.y - b.y
+    };
+    (p.x.abs() + p.y.abs()) as usize
+}
+
+fn parse_input(input: String) -> Vec<Point> {
+    let mut ret = Vec::new();
+
+    let re = Regex::new(r"(\d+), (\d+)").expect("Failed to compile regex");
+    for line in input.lines() {
+        let cap = re.captures_iter(line).next().unwrap();
+        ret.push(Point {x: cap[1].parse().unwrap(), y: cap[2].parse().unwrap()});
+    }
+
+    ret
+}
+
+fn part_1(input: String) -> usize {
+    let input = parse_input(input);
+    let mut counts = vec![0; input.len()];
+    let bounds = Point {
+        x: input.iter().map(|x| x.x).max().unwrap(),
+        y: input.iter().map(|x| x.y).max().unwrap()
+    };
+    let mut banned = Vec::new();
+
+    for y in 0..bounds.y+1 {
+        for x in 0..bounds.x+1 {
+            let p1 = Point {x,y};
+            let mut closest = usize::max_value();
+            let mut closest_i = usize::max_value();
+            let mut previous_closest = closest;
+            for (i, p2) in input.iter().enumerate() {
+                let distance = taxi_dist(&p1, &p2);
+//                println!("{:?}", distance);
+                if distance <= closest {
+                    previous_closest = closest;
+                    closest = distance;
+                    closest_i = i;
+                }
+            }
+
+            if closest == previous_closest {
+                print!(".");
+                continue;
+            }
+            print!("{}", (closest_i + 97) as u8 as char);
+
+            counts[closest_i] += 1;
+            if y == 0 || y == bounds.y || x == 0 || x == bounds.x {
+                banned.push(closest_i);
+            }
+        }
+        println!();
+    }
+
+    let banned_c: std::collections::HashSet<char> = banned.iter().map(|x| (x + 97) as u8 as char).collect();
+    println!("{:?}", banned_c);
+    for (i, c) in counts.iter().enumerate() {
+        println!("{}: {}", (i + 97) as u8 as char, c);
+    }
+
+    *counts.iter().enumerate().filter(|x| !banned.contains(&x.0)).map(|x| x.1).max().unwrap()
+}
+
+fn part_2(input: String) -> String {
+    String::new()
+}
+
+fn main() {
+    run(&part_1, &part_2);
+}
