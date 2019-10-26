ba8ae2b1fa3f9c3e52c9ec756725a1e729095bd4
Delamare2112 <baladashman@gmail.com>
2019-10-26 10:25:27 -0400

    part 2

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -21,37 +21,62 @@ impl Node {
         values_traversed += metadata_len;
         (head, values_traversed)
     }
-    fn sum(&self) -> usize {
+    fn sum_with_children(&self) -> usize {
         let mut sum = self.metadata.iter().sum();
         for child in self.children.iter() {
-           sum += child.sum();
+           sum += child.sum_with_children();
         }
         sum
     }
+    fn value(&self) -> usize {
+        if self.children.is_empty() {
+            self.metadata.iter().sum()
+        } else {
+            let mut value = 0;
+            for i in self.metadata.iter().filter(|&&i| i != 0) {
+                if let Some(node) = self.children.get(i-1) {
+                    value += node.value();
+                }
+            }
+            value
+        }
+    }
 }
 
-fn day_1(input: &str) -> usize {
+fn part_1(input: &str) -> usize {
     let head = Node::new(&input.split_whitespace().map(|s| s.parse().unwrap()).collect());
-    head.sum()
+    head.sum_with_children()
+}
+
+fn part_2(input: &str) -> usize {
+    let head = Node::new(&input.split_whitespace().map(|s| s.parse().unwrap()).collect());
+    head.value()
+}
+
+#[allow(dead_code)] // used in test functions
+const TEST_INPUT: &'static str = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2";
+
+#[test]
+fn part_1_test() {
+    assert_eq!(part_1(TEST_INPUT), 138);
 }
 
 #[test]
-fn day_1_test() {
-    let input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2".to_string();
-    assert_eq!(day_1(input.as_str()), 138);
+fn part_2_test() {
+    assert_eq!(part_2(TEST_INPUT), 66);
 }
 
 fn main() {
     let (day, input) = trev_aoc_common::get_input();
     match day {
         Some(day) => match day {
-            1 => println!("day 1: {}", day_1(input.as_str())),
-//            2 => println!("day 2: {}", day_2(input.as_str())),
+            1 => println!("day 1: {}", part_1(input.as_str())),
+            2 => println!("day 2: {}", part_2(input.as_str())),
             _ => {},
         },
         None => {
-            println!("day 1: {}", day_1(input.as_str()));
-//            println!("day 2: {}", day_2(input.as_str()));
+            println!("day 1: {}", part_1(input.as_str()));
+            println!("day 2: {}", part_2(input.as_str()));
         }
     }
 }
